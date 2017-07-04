e6c73e1ee36fa67df3c67597ffd5fd5454f6e789
Harkamal Singh <harkamalsingh8@outlook.com>
2017-07-04 21:34:16 +0530

    test

--- api/config.php
+++ api/config.php
@@ -1,7 +1,7 @@
 <?php
 
 /*-------- connection parameters ---------*/
-$servername = "148.72.232.174:3306";
+$servername = "148.72.232.174:1433";
 $username = "tayyab@148.72.232.174";
 $password = "Xyky40#6";
 $dbname = "discount";
