d9323403a5171225356f165d584a15da4fa3bec1
Harkamal Singh <harkamalsingh8@outlook.com>
2017-07-04 22:15:49 +0530

    db

--- api/config.php
+++ api/config.php
@@ -2,7 +2,7 @@
 
 /*-------- connection parameters ---------*/
 $servername = "sql11.freemysqlhosting.net:3306";
-$username = "sql11183436@ec2-54-234-208-123.compute-1.amazonaws.com";
+$username = "sql11183436";
 $password = "qrnwsUXter";
 $dbname = "sql11183436";
 
